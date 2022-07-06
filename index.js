f6dea766d2b71ac1ff6b75bee78b63747b6202d2
VL <56016278+myworldbox@users.noreply.github.com>
2022-07-06 17:29:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // prettify json
 app.set("json spaces", 2);
 
 // send alive status as json on '/' endpoint
-app.get("/", (_, res) => {
-  res.json({ alive: true });
+app.get('/', (req, res) => {
+    res.json({ alive: true });
+    console.log("baseUrl")
+});
+
+app.get('/api', (req, res) => {
+    console.log("get api")
+});
+
+app.post('/api', (req, res) => {
+    console.log("post api")
+    req.body.map((data, i) => console.log(i + 1, " -> [", data, " ]"))
 });
 
 app.listen(port, () => {
