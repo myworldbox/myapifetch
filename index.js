f86bbcad9152d1189461d0d2c4b0fa1254eb5b6b
myworldbox <54nicholasleung45@gmail.com>
2022-07-06 23:19:15 +0800

    no message

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-
-const app = express() 
+const app = express()
 
 const port = process.env.PORT || 4000;
 
-// prettify json
-app.set("json spaces", 2);
+var container
 
-app.use(cors({origin: true}));
+app.use(cors());
+app.set("json spaces", 2);
 app.use(bodyParser.json());
 
-// send alive status as json on '/' endpoint
 app.get('/', (req, res) => {
 
-    console.log("get -> [ / ]")
-
+    console.log("\nget -> [ / ]")
     res.json({ route: "/" });
 });
 
 app.get('/api/get', (req, res) => {
 
-    console.log("get -> [ /api/get ]")
-
-    res.json({ route: "/api/get" });
+    console.log("\nget -> [ /api/get ]")
+    res.json(container);
 });
 
 app.post('/api/post', (req, res) => {
 
-    console.log("post -> [ /api/post ]")
+    console.log("\npost -> [ /api/post ]")
+    res.json(req.body);
 
-    res.json({ route: "/api/post" });
-    
-    req.body.map((data, i) => console.log(i + 1, " -> [", data, " ]"))
+    container = req.body
+
+    req.body.map((data, i) => console.log("( ",i + 1, " ) - >>", data, " <<"))
 });
 
 app.listen(port, () => {
