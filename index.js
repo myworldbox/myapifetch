f5b9f9cf4fa56621071ec050ece30a48d3c513e2
myworldbox <54nicholasleung45@gmail.com>
2022-07-06 17:33:57 +0800

    no message

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const express = require("express");
-const app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+
 const port = process.env.PORT || 4000;
 
 // prettify json
 app.set("json spaces", 2);
 
+app.use(cors());
+app.use(bodyParser.json());
+
 // send alive status as json on '/' endpoint
 app.get('/', (req, res) => {
+    console.log("get -> [ / ]")
     res.json({ alive: true });
-    console.log("baseUrl")
 });
 
 app.get('/api', (req, res) => {
-    console.log("get api")
+    console.log("get -> [ /api ]")
 });
 
 app.post('/api', (req, res) => {
-    console.log("post api")
+    console.log("post -> [ /api ]")
     req.body.map((data, i) => console.log(i + 1, " -> [", data, " ]"))
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
